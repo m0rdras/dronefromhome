d2696da7232f26cefd9360ab9d7df43c237e1a4a
Stefan Kirsch <stefan.kirsch@tngtech.com>
2012-10-08 18:53:37 +0200

    minor refactoring

--- app.js
+++ app.js
@@ -2,22 +2,46 @@ var express = require('express'),
     arDrone = require('ar-drone'),
     fs = require('fs'),
     path = require('path'),
-    ffmpeg = require('fluent-ffmpeg'),
     http = require('http');
-
-var client = arDrone.createClient();
 
 var app = module.exports = express(),
     server = http.createServer(app),
     io = require('socket.io').listen(server);
 
-var PORT = 8088;
-
-var CONTROL_TIME = 10;
-
-var pngStream;
+var PORT = 8088,
+    SPEED = 0.5,
+    CONTROL_TIME = 10;
 
 var currentImage = '';
+
+var client = arDrone.createClient();
+var pngStream = client.createPngStream();
+
+pngStream.on('data', function (data) {
+    currentImage = data.toString('base64');
+    console.log('received image');
+});
+
+var actionMap = {
+    // w
+    87: function () { client.front(SPEED); },
+    // s
+    83: function () { client.back(SPEED); },
+    // a
+    65: function () { client.left(SPEED); },
+    // d
+    68: function () { client.right(SPEED); },
+    // space
+    32: function () { client.land(); },
+    // shift
+    16: function () { client.takeoff(); },
+    // c
+    67: function () { client.stop(); },
+    // q
+    81: function () { client.up(SPEED); },
+    // e
+    91: function () { client.down(SPEED); }
+};
 
 var control = {
     x: 0,
@@ -30,10 +54,9 @@ app.configure(function () {
     app.set('views', __dirname + '/views');
     app.set('view engine', 'jade');
     app.set('view options', {
-        layout:false
+        layout: false
     });
     app.use(express.favicon());
-    app.use(express.compress());
     app.use(express.static(__dirname + '/public'));
     app.use(express.bodyParser());
     app.use(express.methodOverride());
@@ -41,24 +64,20 @@ app.configure(function () {
 });
 
 app.configure('development', function () {
-    app.use(express.errorHandler({ dumpExceptions:true, showStack:true }));
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
 app.configure('production', function () {
     app.use(express.errorHandler());
 });
 
-
-function sendStream(req, res) {
-    res.json({ base64image: currentImage });
-}
-
-
 app.get('/', function (req, res) {
     res.render('index');
 });
 
-app.get('/stream', sendStream);
+app.get('/stream', function (req, res) {
+    res.json({ base64image: currentImage });
+});
 
 io.sockets.on('connection', function (socket) {
     //setInterval(function() {
@@ -66,44 +85,10 @@ io.sockets.on('connection', function (socket) {
     //  socket.emit('heartbeat', "test");
     //}, 1000);
     socket.on('command', function (data) {
-        console.log(data);
-        if (data == 87) {
-            console.log("w pushed");
-            client.front(0.5);
-        }
-        if (data == 83) {
-            console.log("s pushed");
-            client.back(0.5);
-        }
-        if (data == 65) {
-            console.log("a pushed");
-            client.left(0.5);
-        }
-        if (data == 68) {
-            console.log("d pushed");
-            client.right(0.5);
-        }
-        if (data == 32) {
-            console.log("SPACE pushed");
-            client.land();
-        }
-        if (data == 16) {
-            console.log("SHIFT pushed");
-            client.takeoff();
-        }
-        if (data == 67) {
-            console.log("c pushed");
-            client.stop();
-        }
-        if (data == 81) {
-            console.log("q pushed");
-            client.up(0.5);
-        }
-        if (data == 91) {
-            console.log("e pushed");
-            client.down(0.5);
-        }
+        console.log('got drone command: ' + data);
+        actionMap[data].apply();
     });
+
     socket.on('mouseX', function (data) {
         console.log('mouse x: ' + data);
         if (data < 0) {
@@ -113,8 +98,10 @@ io.sockets.on('connection', function (socket) {
         }
         control.rot = CONTROL_TIME;
     });
+
     socket.on('mouseY', function (data) {
         console.log('mouse y: ' + data);
+
         if (data < 0) {
             client.up(0.5);
         } else {
@@ -125,33 +112,5 @@ io.sockets.on('connection', function (socket) {
 });
 
 var application = server.listen(PORT);
-pngStream = client.createPngStream();
 
-pngStream.pause = function () {
-};
-pngStream.destroy = function () {
-};
-
-pngStream.on('data', function (data) {
-    currentImage = data.toString('base64');
-    console.log('received image');
-});
-
-
-console.log("Express server listening on port %s in %s mode", PORT, app.settings.env);
-
-//setInterval(function () {
-//    if (control.rot > 1) {
-//        control.rot -= 1;
-//    } else if (control.rot === 1) {
-//        control.rot = 0;
-//        client.stop();
-//    }
-//
-//    if (control.x > 1) {
-//        control.x -= 1;
-//    } else if (control.x === 1) {
-//        control.x = 0;
-//        client.stop();
-//    }
-//}, 100);
+console.log("DroneFromHome server listening on port %s in %s mode", PORT, app.settings.env);
